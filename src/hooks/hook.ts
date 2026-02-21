fecf4fdce06749cfee86acdd1ec4eb2f864ad06a
christian camilo schottlaender Castro <camischo@gmail.com>
2026-02-21 06:19:12 -0500

    take screenshot after scenario failed

--- src/hooks/hook.ts
+++ src/hooks/hook.ts
@@ -1,4 +1,4 @@
-import { BeforeAll, AfterAll } from "@cucumber/cucumber";
+import { BeforeAll, After, Status } from "@cucumber/cucumber";
 import { Page, Browser} from 'playwright';
 import { paginaLogin } from '../pages/Login.page';
 import { invokeBrowser } from "./browserManager";
@@ -10,4 +10,15 @@ BeforeAll(async function () {
     browser = await invokeBrowser();
     page = await browser.newPage();
     paginaLogin.page = page;
+});
+
+After(async function (scenario) {
+  if (scenario.result?.status === Status.FAILED) {
+    const screenshot = await page.screenshot({ 
+      path: `./reports/screenshots/${scenario.pickle.name}.png`, 
+      fullPage: true 
+    });
+    
+    this.attach(screenshot, 'image/png');
+  }
 });
